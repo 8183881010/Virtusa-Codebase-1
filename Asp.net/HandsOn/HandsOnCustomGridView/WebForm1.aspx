9c48225d994123cea076131284ef101faec289c9
santuparsi <santuparsi@gmail.com>
2022-09-19 15:53:40 +0530

    new_commit919

---
+++ Asp.net/HandsOn/HandsOnCustomGridView/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="HandsOnCustomGridView.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" OnRowCommand="GridView1_RowCommand">
+    <Columns>
+        <asp:TemplateField HeaderText="Name" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:TextBox ID="txtName" runat="server" Text='<%# Eval("Name") %>' />
+            </ItemTemplate>
+        </asp:TemplateField>
+         <asp:TemplateField HeaderText="Price" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:DropDownList ID="ddlPrice" runat="server">
+                    <asp:ListItem>12</asp:ListItem>
+                    <asp:ListItem>13</asp:ListItem>
+                    <asp:ListItem>14</asp:ListItem>
+                    <asp:ListItem>15</asp:ListItem>
+                </asp:DropDownList>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:BoundField DataField="Country" HeaderText="Country" ItemStyle-Width="150px" />
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:Button Text="Select" runat="server" CommandName="Select" CommandArgument="<%# Container.DataItemIndex %>" />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Asp.net/HandsOn/HandsOnCustomGridView/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace HandsOnCustomGridView
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Country") });
+                dt.Rows.Add(1, "John Hammond", "United States");
+                dt.Rows.Add(2, "Mudassar Khan", "India");
+                dt.Rows.Add(3, "Suzanne Mathews", "France");
+                dt.Rows.Add(4, "Robert Schidner", "Russia");
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+{
+    if (e.CommandName == "Select")
+    {
+        //Determine the RowIndex of the Row whose Button was clicked.
+        int rowIndex = Convert.ToInt32(e.CommandArgument);
+ 
+        //Reference the GridView Row.
+        GridViewRow row = GridView1.Rows[rowIndex];
+ 
+        //Fetch value of Name.
+        string name = (row.FindControl("txtName") as TextBox).Text;
+                //Fetch value of Price.
+                string Price = (row.FindControl("ddlPrice") as DropDownList).Text;
+                //Fetch value of Country
+                string country = row.Cells[1].Text;
+ 
+        ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Name: " + name + "\\nCountry: " + country + "Price: "+Price+"');", true);
+    }
+}
+    }
+}
